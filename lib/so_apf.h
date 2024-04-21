7b4c928ec8923fb070f759ed75a64e5b8a116c0c
crylent Maxim Belkovsky <inkerimaa1@gmail.com>
2024-04-22 01:55:18 +0300

    second order APF formulas fix

--- lib/so_apf.h
+++ lib/so_apf.h
@@ -6,18 +6,17 @@
 * Q, steepness of phase shift at fc (second-order only)
 */
 #pragma once
-#include "biquad.h"
+#include "biquad_q.h"
 
-class SO_APF : public Biquad {
+class SO_APF : public BiquadQ {
+    using BiquadQ::BiquadQ;
+
 public:
-    SO_APF(float Q, int fs) {
-        calculate_coeffs(Q, fs);
-    }
-
-    tp_coeffs& calculate_coeffs(float Q, int fs)
+    tp_coeffs& calculate_coeffs(float Q, int fc, int fs)
     {
-        coef_size_t a = (tan(pi*Q / fs) - 1.0) / (tan(pi*Q / fs) + 1.0);
-        coef_size_t b = -cos(pi*Q / fs);
+        coef_size_t bw = fc / Q;
+        coef_size_t a = (tan(pi*bw / fs) - 1.0) / (tan(pi*bw / fs) + 1.0);
+        coef_size_t b = -cos(pi*fc / fs);
         m_coeffs.a0 = -a;
         m_coeffs.a1 = b*(1.0 - a);
         m_coeffs.a2 = 1.0;
